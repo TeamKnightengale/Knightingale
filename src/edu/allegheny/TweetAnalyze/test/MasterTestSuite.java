b06c90b81c61fb6d407c96ab27d070e261ae1e35
Hawk Weisman <hawk.weisman@gmail.com>
2013-11-17 14:53:16 -0500

    Added edu.allegheny.TweetAnalyze.test and MasterTestSuite

---
+++ src/edu/allegheny/TweetAnalyze/test/MasterTestSuite.java
@@ -0,0 +1,21 @@
+package edu.allegheny.TweetAnalyze.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Test suite for TweetAnalyze, invokes all test cases
+ *
+ * @author 		Hawk Weisman
+ * @version 	0.1
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+
+TestTweet.class,
+// ADD YOUR TEST CLASSES HERE
+
+})
+
+public class MasterTestSuite {
+}
