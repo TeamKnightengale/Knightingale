c1017350e9050900a0597c512ec2a14127295642
Hawk Weisman <hawk.weisman@gmail.com>
2013-11-20 15:38:55 -0500

    Begin adding methods to ZipParser

--- src/edu/allegheny/TweetAnalyze/Parser/ZipParser.java
+++ src/edu/allegheny/TweetAnalyze/Parser/ZipParser.java
@@ -8,11 +8,19 @@
  *
  */
 
-package src.edu.allegheny.TweetAnalyze.Parser;
+package .edu.allegheny.TweetAnalyze.Parser;
 
 import	java.io.File;
 import 	java.nio.File;
 import	java.util.ArrayList;
+
+import edu.allegheny.TweetAnalyze.Tweet;
+
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Contains methods for expanding zipped Twitter archives, locating Tweet CSV files, and controlling the Twitter archive import process.
@@ -22,6 +30,9 @@ import	java.util.ArrayList;
  * @see		edu.allegheny.TweetAnalyze.Parser.CSVParser
  */
 public class ZipParser {
+
+	private static Logger logger = LogManager.getFormatterLogger();
+
 	
 	public ZipParser () {
 
@@ -33,7 +44,34 @@ public class ZipParser {
 	 * @param	target	a java.io.File object containing a path to the target Zip file.
 	 * @return	an ArrayList containing the Tweets from the Tweets.csv file in the target Zip.
 	 */
-	public parse (File target) {
+	public ArrayList<Tweet> parse (File target) throws ZipException {
 
+		try {
+				if (logger.isDebugEnabled()) {
+					logger.debug("ZipParser: Target file is %s", target);
+				}
+		
+				ZipFile archive = new ZipFile(target);
+		
+				extractTweetsCSV(archive);
+
+				if (logger.isDebugEnabled()) {
+					logger.debug("ZipParser: Tweets.csv extracted, passing control to CSVParser");
+				}
+		
+				return CSVParser.parseTweets();
+
+		} catch (ZipException e) {
+
+		}
+	}
+
+	private void extractTweetsCSV (ZipFile target) throws ZipException {
+
+		if (logger.isDebugEnabled()) {
+			logger.debug("ZipParser: extracting Tweets.csv from %s", target);
+		}
+
+		target.extractFile("tweets.csv")
 	}
 }
