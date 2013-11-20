95ebe3c5d0f5184a93d3b883045f5d08406e0d27
Hawk Weisman <hawk.weisman@gmail.com>
2013-11-20 15:09:30 -0500

    Begin adding method signatures and documentation

--- src/edu/allegheny/TweetAnalyze/Parser/ZipParser.java
+++ src/edu/allegheny/TweetAnalyze/Parser/ZipParser.java
@@ -1,5 +1,39 @@
+/*
+ *  _____                    _     _               _               
+ * /__   \__      _____  ___| |_  /_\  _ __   __ _| |_   _ _______ 
+ *  / /\/\ \ /\ / / _ \/ _ \ __|//_\\| '_ \ / _` | | | | |_  / _ \
+ * / /    \ V  V /  __/  __/ |_/  _  \ | | | (_| | | |_| |/ /  __/
+ * \/      \_/\_/ \___|\___|\__\_/ \_/_| |_|\__,_|_|\__, /___\___|
+ * Open-source Twitter analytics    		        |___/    
+ *
+ */
+
 package src.edu.allegheny.TweetAnalyze.Parser;
 
+import	java.io.File;
+import 	java.nio.File;
+import	java.util.ArrayList;
+
+/**
+ * Contains methods for expanding zipped Twitter archives, locating Tweet CSV files, and controlling the Twitter archive import process.
+ *
+ * @author	Hawk Weisman
+ * @version	0.1
+ * @see		edu.allegheny.TweetAnalyze.Parser.CSVParser
+ */
 public class ZipParser {
 	
+	public ZipParser () {
+
+	}
+
+	/**
+	 * Parse: runs the complete parsing process.
+	 * 
+	 * @param	target	a java.io.File object containing a path to the target Zip file.
+	 * @return	an ArrayList containing the Tweets from the Tweets.csv file in the target Zip.
+	 */
+	public parse (File target) {
+
+	}
 }
