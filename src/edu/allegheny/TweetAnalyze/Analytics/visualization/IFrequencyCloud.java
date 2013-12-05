f94f6b0ed5696e532462f179d23dea5b118c749f
Hawk Weisman <hawk.weisman@gmail.com>
2013-12-04 19:43:22 -0500

    Add rudimentary cloud implementation, will refactor

---
+++ src/edu/allegheny/TweetAnalyze/Analytics/visualization/IFrequencyCloud.java
@@ -0,0 +1,15 @@
+package edu.allegheny.TweetAnalyze.analytics.visualization;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import org.mcavallo.opencloud.Cloud;
+import org.mcavallo.opencloud.Tag;
+
+public interface IFrequencyCloud {
+
+	public void open();
+
+}
