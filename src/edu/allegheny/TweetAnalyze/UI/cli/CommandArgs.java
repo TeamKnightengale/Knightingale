a9e9eba54613c9c81c6ede10ae0bf071eb5dec49
Dibyo Mukherkee <dibyo.m@gmail.com>
2013-12-12 11:47:28 -0500

    Add v1 add command

--- src/edu/allegheny/TweetAnalyze/UI/cli/CommandArgs.java
+++ src/edu/allegheny/TweetAnalyze/UI/cli/CommandArgs.java
@@ -1,19 +1,32 @@
 package edu.allegheny.tweetanalyze.ui.cli;
 
 import com.beust.jcommander.*;
+import java.util.List;
+import java.io.File;
 
 public class CommandArgs
 {
+	@Parameters(commandDescription = "Add tweets from your tweet archive")
+	public class AddCommand
+	{
+		@Parameter(description = "Your twitter archive zip file")
+		List<String> file;
+
+		public String getFile()
+		{
+			return file.get(0);
+		}
+	}
 
 	@Parameters(commandDescription = "Searches for all tweets with a word")
 	public class SearchCommand
 	{
 		@Parameter(description = "words to search for")
-		private String search;	
+		private List<String> search;	
 
 		public String getSearch()
 		{
-			return search;
+			return search.get(0);
 		}
 	}
 	
@@ -23,8 +36,4 @@ public class CommandArgs
 
 	}
 
-	public class TokenCommand
-	{
-
-	}
 }
