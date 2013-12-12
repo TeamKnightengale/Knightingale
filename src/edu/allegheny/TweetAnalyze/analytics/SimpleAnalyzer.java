b780bb902e83c455bde7ae4c8a9d923414732742
Hawk Weisman <hawk.weisman@gmail.com>
2013-12-11 23:30:49 -0500

    Add documentation

--- src/edu/allegheny/TweetAnalyze/analytics/SimpleAnalyzer.java
+++ src/edu/allegheny/TweetAnalyze/analytics/SimpleAnalyzer.java
@@ -1,3 +1,10 @@
+//     __ __     _      __   __  _                __   
+//    / //_/__  (_)__ _/ /  / /_(_)__  ___ ____ _/ /__ 
+//   / ,< / _ \/ / _ `/ _ \/ __/ / _ \/ _ `/ _ `/ / -_)
+//  /_/|_/_//_/_/\_, /_//_/\__/_/_//_/\_, /\_,_/_/\__/ 
+//              /___/                /___/          
+//  Open-source Twitter analytics...with style!
+
 package edu.allegheny.TweetAnalyze.analytics;
 
 import edu.allegheny.TweetAnalyze.Tweet;
